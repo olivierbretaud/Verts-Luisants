5c0fa8f313bf6b2ac8f1eeae9c3ec0ef09961531
olivierbretaud <olivierbretaud@hotmail.fr>
2018-09-24 22:13:19 +0200

    first commit

---
+++ themes/vertsluisants/header.php
@@ -0,0 +1,47 @@
+<?php if (!defined('PLX_ROOT')) exit; ?>
+<!DOCTYPE html>
+<html lang="<?php $plxShow->defaultLang() ?>">
+<head>
+	<meta charset="<?php $plxShow->charset('min'); ?>">
+	<meta name="viewport" content="width=device-width, user-scalable=yes, initial-scale=1.0">
+	<title><?php $plxShow->pageTitle(); ?></title>
+	<?php $plxShow->meta('description') ?>
+	<?php $plxShow->meta('keywords') ?>
+	<?php $plxShow->meta('author') ?>
+	<link rel="icon" href="<?php $plxShow->template(); ?>/img/logo.png" />
+	<link rel="stylesheet" href="<?php $plxShow->template(); ?>/css/plucss.css" media="screen"/>
+	<link rel="stylesheet" href="<?php $plxShow->template(); ?>/css/theme.css" media="screen"/>
+	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.3.1/css/all.css" integrity="sha384-mzrmE5qonljUremFsqc01SB46JvROS7bZs3IO2EmfFsd15uHvIt+Y8vEf7N7fWAU" crossorigin="anonymous">
+	<link href="https://fonts.googleapis.com/css?family=Paytone+One" rel="stylesheet">
+	<?php $plxShow->templateCss() ?>
+	<?php $plxShow->pluginsCss() ?>
+	<link rel="alternate" type="application/rss+xml" title="<?php $plxShow->lang('ARTICLES_RSS_FEEDS') ?>" href="<?php $plxShow->urlRewrite('feed.php?rss') ?>" />
+	<link rel="alternate" type="application/rss+xml" title="<?php $plxShow->lang('COMMENTS_RSS_FEEDS') ?>" href="<?php $plxShow->urlRewrite('feed.php?rss/commentaires') ?>" />
+</head>
+
+<body id="top" class="page mode-<?php $plxShow->mode(true) ?>">
+
+	<nav class="nav">
+		<div class="container menu-box">
+			<div class ="logo-box">
+				<div class="logo-img"></div>
+				<span class="logo-text">Verts-Luisants</span>
+			</div>
+
+			<div class="responsive-menu">
+				<label for="menu"><i class="fas fa-bars"></i></label>
+				<input type="checkbox" id="menu">
+				<ul class="menu">
+					<?php $plxShow->staticList($plxShow->getLang('HOME'),'<li class="#static_class #static_status" id="#static_id"><a href="#static_url" title="#static_name">#static_name</a></li>'); ?>
+					<?php $plxShow->pageBlog('<li class="#page_class #page_status" id="#page_id"><a href="#page_url" title="#page_name">#page_name</a></li>'); ?>
+				</ul>
+			</div>
+		</div>
+	</nav>
+
+	<header class="header">
+		<div class="container">
+			<h1 class="no-margin heading-small"><?php $plxShow->mainTitle(); ?></h1>
+			<h2 class="h5 no-margin"><?php $plxShow->subTitle(); ?></h2>
+		</div>
+	</header>
